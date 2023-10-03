7e9aa3084127f104d64a7d193370c3f3f67a1243
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-10-03 09:50:43 +0200

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,11 @@ function Footer() {
         <footer>
             <div className="autoWidth">
                 <p>
-                    Sida skapad av <a href="https://github.com/Hjalmar-Lundmark">Hjalmar Lundmark</a> | <a href="https://github.com/Hjalmar-Lundmark/te4-weather">Github repo</a>.
+                    Sida skapad av <a href="https://github.com/Hjalmar-Lundmark">Hjalmar Lundmark</a> | <a href="https://github.com/Hjalmar-Lundmark/te4-shop-prototype">Github repo</a>. <br />
+                    Ångerrätt gäller i 14 dagar från leverans. <br />
+                    Alla priser är inklusive moms. <br />
+                    Vi använder cookies för att förbättra din upplevelse på vår hemsida. <br />
+                    <a href="">Kontakta oss</a> om du har några frågor.
                 </p>
             </div>
         </footer>
