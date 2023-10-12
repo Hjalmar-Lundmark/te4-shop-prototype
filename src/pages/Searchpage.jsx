3d83263e69ddeb1c9a5d98e42345ee1a1507ebc7
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-10-12 14:31:14 +0200

    Working on search

--- src/pages/Searchpage.jsx
+++ src/pages/Searchpage.jsx
@@ -1,24 +1,49 @@
-import { useState, useParams } from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import ProductCard from '../components/ProductCard'
 
 function Searchpage() {
-    const { product, setProduct } = useState([])
+    const { searchProducts, setSearchProducts } = useState([])
     const { value } = useParams()
 
     function fetchSearch() {
+        console.log(value)
         fetch(`http://localhost:3000/product/search/${value}`)
             .then(res => res.json())
             .then(result => {
                 console.log(result)
-                setProduct(result)
+                setSearchProducts(result.data)
             }).catch(err => {
                 console.log(err)
             });
     }
 
+    useEffect(() => {
+        fetchSearch()
+    }, [])
+
     return (
-        <div>
+        <>
             <h1>Searchpage</h1>
-        </div>
+            <ul className='allWares'>
+                {searchProducts.length > 0 ? (
+                    <>
+                        {searchProducts.map((item) => (
+                            <ProductCard
+                                key={item.id}
+                                id={item.id}
+                                img={'../' + item.image}
+                                name={item.name}
+                                price={item.price}
+                                oldPrice={item.oldPrice}
+                            />
+                        ))}
+                    </>
+                ) :
+                    <p>Problem med att ladda in produkter</p>
+                }
+            </ul>
+        </>
     )
 }
 
