40ea0fa361ae95220d00fe55bcc4a855b5af9857
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-10-13 10:50:33 +0200

    Update ReadMe

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 5. Custom popup för när vara stoppas i korgen
 
 ## Att göra backend
-1. Totalt värde och totala produkter i varukorg
 4. Varukorg kan ha 2 av en produkt istället för produkten 2 ggr
-3. Säkra databas bättre
+3. Säkra databas bättre?
 5. Take och skip i db?
+5. Fixa knappar i checkout
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -53,10 +53,10 @@ function Cart() {
                         <CartItem
                             key={index}
                             id={item.id}
-                            name={item.name || 'Product'} // where should I get these from?
+                            name={item.name}
                             deleteItem={deleteItem}
-                            img={item.img || 'dog5.jpg'}
-                            price={item.price || 100}
+                            img={item.img}
+                            price={item.price}
                         />
                     )}
                 </div>
