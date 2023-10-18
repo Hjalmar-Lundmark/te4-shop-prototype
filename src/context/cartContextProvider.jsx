73a993bfe6c2f02386a429767ac4bb5efc67a2d0
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-10-18 09:46:13 +0200

    Start on context

---
+++ src/context/cartContextProvider.jsx
@@ -0,0 +1,136 @@
+import { createContext, useState, useEffect } from "react";
+import { useLocation } from 'react-router-dom'
+
+export const CartContext = createContext();
+
+export const CartContextProvider = ({ children }) => {
+    const [cartItems, setCartItems] = useState(() => {
+        return JSON.parse(localStorage.getItem('cartItems')) || [];
+    });
+    const location = useLocation();
+
+    const [totalPrice, setTotalPrice] = useState(0)
+    const [totalItems, setTotalItems] = useState(0)
+
+    const deleteItem = (id) => {
+        setCartItems(JSON.parse(localStorage.getItem('cartItems')))
+        var newCartItems
+
+        if (cartItems.find(item => item.id === id && item.amount > 1)) {
+            newCartItems = cartItems.map(item => {
+                if (item.id === id && item.amount > 1) {
+                    item.amount -= 1
+                }
+                return item
+            })
+        } else {
+            newCartItems = cartItems.filter(item => item.id !== id)
+        }
+
+        setCartItems(newCartItems)
+        localStorage.setItem('cartItems', JSON.stringify(newCartItems));
+    }
+
+    const deleteAll = () => {
+        setCartItems([]);
+        localStorage.setItem('cartItems', JSON.stringify([]));
+    }
+
+    useEffect(() => {
+        let price = 0
+        for (let i = 0; i < cartItems.length; i++) {
+            price += (cartItems[i].price * cartItems[i].amount)
+        }
+        setTotalPrice(price)
+    }, [cartItems])
+
+    function getTotalPrice() {
+        let price = 0
+        for (let i = 0; i < cartItems.length; i++) {
+            price += (cartItems[i].price * cartItems[i].amount)
+        }
+        setTotalPrice(price)
+    }
+
+    function getTotalItems() {
+        let items = 0
+        for (let i = 0; i < cartItems.length; i++) {
+            items += 1
+        }
+        setTotalItems(items)
+    }
+
+    function flipCart() {
+        if (location.pathname === '/checkout') {
+            return document.getElementById('cart').style.display = 'none'
+        }
+
+        if (document.getElementById('cart').style.display === 'block') {
+            document.getElementById('cart').style.display = 'none'
+        } else {
+            document.getElementById('cart').style.display = 'block'
+        }
+    }
+
+    const addItem = (id, name, img, price) => {
+        setCartItems(JSON.parse(localStorage.getItem('cartItems')))
+        let newCartItems
+
+        if (cartItems.find(item => item.id === id)) {
+            console.log('Produkten finns redan i varukorgen')
+            newCartItems = cartItems.map(item => {
+                if (item.id === id) {
+                    item.amount += 1
+                }
+                return item
+            })
+        } else {
+            newCartItems = [...cartItems, { id: id, name: name, img: img, price: price, amount: 1 }]
+        }
+
+        console.log(cartItems)
+        console.log(newCartItems)
+        if (cartItems === newCartItems) {
+            message('Något gick fel, försök igen', 'warning')
+        } else {
+            message(name + ' lades till i varukorgen', 'success')
+        }
+
+        localStorage.setItem('cartItems', JSON.stringify(newCartItems));
+    }
+
+    function message(text, type) {
+        const msgbox = document.querySelector('#msgbox');
+
+        const p = document.createElement('p');
+        p.classList.add(type);
+        p.textContent = text;
+        msgbox.appendChild(p);
+        setTimeout(() => {
+            p.parentNode.removeChild(p);
+        }, 2500);
+    }
+
+    return (
+        <CartContext.Provider value={{
+            cartItems,
+            addItem,
+            deleteItem,
+            deleteAll,
+            flipCart,
+            message,
+            getTotalPrice,
+            getTotalItems,
+            // clearCart,
+            // isInCart,
+            // getTotalItems,
+            // getTotalPrice,
+            // increaseQuantity,
+            // decreaseQuantity
+        }}>
+            {children}
+        </CartContext.Provider>
+    );
+}
+
+// Path: src/components/Cart.jsx
